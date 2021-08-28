95483e442479c34e137e6c34aca9aee2e31e8538
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-08-29 01:27:30 +0500

    :memo: Added an index page describing what the site is about (#5)

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from scraper import IndeedScraper, SalaryType
 
 app = FastAPI()
 
 
+app.mount("/static", StaticFiles(directory="assets"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/salary/")
