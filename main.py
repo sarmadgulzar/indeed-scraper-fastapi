1015d1c3684a6686aefca2f060e418857f044d95
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-08-29 00:50:16 +0500

    :sparkles: Added Indeed salary scraper (#2)

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from fastapi import FastAPI
+
+from scraper import IndeedScraper, SalaryType
 
 app = FastAPI()
 
@@ -6,3 +10,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("/salary/")
+async def salary(salary_type: SalaryType, job_title: str, location: Optional[str] = ""):
+    scraper = IndeedScraper(salary_type=salary_type)
+    salary = scraper.scrape(job_title=job_title, location=location)
+    return {"salary": salary}
